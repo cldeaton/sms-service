6819cdd0675fce5e0a81105d8cf20413cdfe5029
Chris Deaton <chrisdeaton@Chriss-MacBook-Pro.local>
2018-07-24 19:59:16 -0400

    updates: minor

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
 const twilio            = require('twilio'),
-      http              = require('http'),
-      express           = require('express'),
-      app               = express(),
-      MessagingResponse = require('twilio').twiml.MessagingResponse,
       accountSid        = process.env.ACCOUNT_SID,
       authToken         = process.env.AUTH_TOKEN,
       client            = new twilio(accountSid, authToken),
       userNumber        = process.env.USER_NUMBER,
       twilioNumber      = process.env.TWILIO_NUMBER,
-      messageBody       = 'Your phone has now been taken over. Your mission, should you choose to accept it, is to reply to this text message.';
+      messageBody       = process.env.MESSAGE_BODY;
 
 // Creates outgoing message
 client.messages.create({
@@ -17,24 +13,3 @@ client.messages.create({
     from: twilioNumber
 })
 .then((message) => console.log(message.sid));
-
-
-// Sending out a sms response via webhook
-app.post('/sms', (req, res) => {
-  const twiml = new MessagingResponse();
-
-  twiml.message('Thank you, your phone will now self destruct in 5 seconds');
-  // twiml.message('5');
-  // twiml.message('4');
-  // twiml.message('3');
-  // twiml.message('2');
-  // twiml.message('1');
-  // twiml.message('Boom!');
-
-  res.writeHead(200, {'Content-Type': 'text/xml'});
-  res.end(twiml.toString());
-});
-
-http.createServer(app).listen(1337, () => {
-  console.log('Express server listening on port 1337');
-});
