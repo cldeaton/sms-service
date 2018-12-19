831a9bb81f50a2607fb48bb8afd545d6137234bf
Chris Deaton <cldeaton24@gmail.com>
2018-12-18 19:21:30 -0500

    api ref added/post request built

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # sms-service
+
+#API-REF
+
+##Description:
+SMS-service is a node micro-service that receives information in the form of a post request made through its api and sends that information in a text message to a number provided by the user.
+
+##Routes:
+
+###Send SMS
+
+POST /sms
+  body:
+    messageBody: The text or information that the user wishes to share.
+    userNumber: The number the user wishes to send the message to.
+    userSecret: An encrypted signal that the user has the appropriate permission to send a request to the api.
+
+##Examples:
--- server.js
+++ server.js
@@ -1,30 +1,61 @@
-let express      = require("express"),
-    app          = express(),
-    bodyParser   = require('body-parser'),
-    twilio       = require('twilio'),
-    accountSid   = process.env.ACCOUNT_SID,
-    authToken    = process.env.AUTH_TOKEN,
-    client       = new twilio(
+const express = require("express");
+const app = express();
+const bodyParser = require('body-parser');
+const twilio = require('twilio');
+const crypto = require('crypto');
+const cipher = crypto.createCipher('aes192', 'a password');
+const decipher = crypto.createDecipher('aes192', 'a password');
+
+let accountSid = process.env.ACCOUNT_SID;
+let authToken = process.env.AUTH_TOKEN;
+let client = new twilio(
       accountSid,
       authToken
-    ),
-    twilioNumber = process.env.TWILIO_NUMBER,
-    userNumber   = process.env.USER_NUMBER,
-    messageBody  = process.env.MESSAGE_BODY;
+    );
+let twilioNumber = process.env.TWILIO_NUMBER;
+let userSecret = process.env.USER_SECRET;
+// let secretMessage = 1234567890 + "," + userSecret;
+// let encrypted = cipher.update(secretMessage, 'utf8', 'hex');
+// encrypted += cipher.final('hex');
+// let decrypted = decipher.update(encrypted, 'hex', 'utf8');
+// decrypted += decipher.final('utf8');
+// let passcode = decrypted.split(",")[1];
 
+console.log(userSecret);
+// console.log(encrypted);
+// console.log(decrypted);
+// console.log(passcode);
 
 
 app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.post("/", function(req, res){
-  console.log(messageBody, userNumber)
-  //logic for authentication...
+  let userNumber = req.body.userNumber;
+  let messageBody = req.body.messageBody;
+  let secret = req.body.userSecret;
+  let secretMessage = userNumber + "," + secret;
+  let encrypted = cipher.update(secretMessage, 'utf8', 'hex');
+  encrypted += cipher.final('hex');
+  let decrypted = decipher.update(encrypted, 'hex', 'utf8');
+  decrypted += decipher.final('utf8');
+  let passcode = decrypted.split(",")[1];
+
+  console.log(messageBody, userNumber, secret);
+  // console.log(secret);
+  console.log(encrypted);
+  console.log(decrypted);
+  console.log(secretMessage);
+  console.log(passcode);
+
+if(passcode === userSecret){
   client.messages.create({
       body: messageBody,
       to: userNumber,
       from: twilioNumber
   })
   .then((message) => console.log(message.sid));
+}
 })
 
 
